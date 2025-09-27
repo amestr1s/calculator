be492ae6bf645634416b154ff62ede6a56a6d830
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-27 20:30:30 +0300

    Create initial files, add basic functions

---
+++ js.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+// function sum(arr) {
+// 	let result = arr.reduce((sum, current) => sum + current, 0);
+//   return result;
+// };
+
+// function subtractionSum(arr) {
+// 	let result = arr.reduce((sum, current) => sum - current, 0);
+//   return result;
+// };
+
+// function multiply(arr) {
+//   let result = arr.reduce((product, current) => product * current);
+//   return result;
+// };
+
+// function divide(arr) {
+//   let result = arr.reduce((product, current) => product / current);
+//   return result;
+// };
