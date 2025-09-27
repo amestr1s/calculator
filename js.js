285938efdbace4f2760e46485b1ccc9754047146
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-27 20:49:25 +0300

    Make main function to call basic functions

--- js.js
+++ js.js
@@ -13,6 +13,37 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let numA;
+let operator;
+let numB;
+
+function operate (numA, operator, numB) {
+    if (operator == "+") {
+      let result = add(numA, numB);
+        console.log(result);
+    } else if (operator == "-") {
+      let result = subtract(numA, numB);
+        console.log(result);
+    } else if (operator == "*") {
+      let result = multiply(numA, numB);
+        console.log(result);
+    } else if (operator == "/") {
+      let result = divide(numA, numB);
+        console.log(result);
+    }
+}
+
+operate(5, "*", 5);
+
+
+
+
+
+
+
+
+
 // function sum(arr) {
 // 	let result = arr.reduce((sum, current) => sum + current, 0);
 //   return result;
