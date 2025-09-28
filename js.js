c8d5deea29c62b151b08e238f779bb0a5897fcb1
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-28 11:30:14 +0300

    Add eventlisteners to digits

--- js.js
+++ js.js
@@ -17,24 +17,139 @@ function divide(a, b) {
 let numA;
 let operator;
 let numB;
+let result;
+let displayContent;
 
 function operate (numA, operator, numB) {
     if (operator == "+") {
-      let result = add(numA, numB);
+      result = add(numA, numB);
         console.log(result);
     } else if (operator == "-") {
-      let result = subtract(numA, numB);
+      result = subtract(numA, numB);
         console.log(result);
     } else if (operator == "*") {
-      let result = multiply(numA, numB);
+      result = multiply(numA, numB);
         console.log(result);
     } else if (operator == "/") {
-      let result = divide(numA, numB);
+      result = divide(numA, numB);
         console.log(result);
     }
 }
 
-operate(5, "*", 5);
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const multiplier = document.querySelector("#multiplier");
+const divider = document.querySelector("#divider");
+const equal = document.querySelector("#equal");
+const dot = document.querySelector("#dot");
+const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#backspace");
+const display = document.querySelector("#display");
+
+
+
+zero.addEventListener('click',() => {
+        display.textContent = "0";
+        displayContent = display.textContent;
+})
+
+one.addEventListener('click',() => {
+        display.textContent = "1";
+        displayContent = display.textContent;
+})
+
+two.addEventListener('click',() => {
+        display.textContent = "2";
+        displayContent = display.textContent;
+})
+
+three.addEventListener('click',() => {
+        display.textContent = "3";
+        displayContent = display.textContent;
+})
+
+four.addEventListener('click',() => {
+        display.textContent = "4";
+        displayContent = display.textContent;
+})
+
+five.addEventListener('click',() => {
+        display.textContent = "5";
+        displayContent = display.textContent;
+})
+
+six.addEventListener('click',() => {
+        display.textContent = "6";
+        displayContent = display.textContent;
+})
+
+seven.addEventListener('click',() => {
+        display.textContent = "7";
+        displayContent = display.textContent;
+})
+
+eight.addEventListener('click',() => {
+        display.textContent = "8";
+        displayContent = display.textContent;
+})
+
+nine.addEventListener('click',() => {
+        display.textContent = "9";
+        displayContent = display.textContent;
+})
+
+plus.addEventListener('click',() => {
+        
+})
+
+minus.addEventListener('click',() => {
+        
+})
+
+multiplier.addEventListener('click',() => {
+        
+})
+
+divider.addEventListener('click',() => {
+        
+})
+
+equal.addEventListener('click',() => {
+        
+})
+
+dot.addEventListener('click',() => {
+        display.textContent = ".";
+})
+
+clear.addEventListener('click',() => {
+        display.textContent = "";
+})
+
+backspace.addEventListener('click',() => {
+        
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
